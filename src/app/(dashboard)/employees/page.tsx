024269ffe1121a1d15fe7d79302e7e8abf9b4386
VietMy <ziettmy@gmail.com>
2024-12-30 15:48:51 +0700

    create layout

--- src/app/(dashboard)/employees/page.tsx
+++ src/app/(dashboard)/employees/page.tsx
@@ -1,9 +1,110 @@
-import React from 'react'
 
-const EmployeePage = () => {
+import Image from "next/image";
+import Link from "next/link";
+import { employeesData  } from "../../../lib/data";
+import Table from "../../../components/Table";
+import Pagination from "../../../components/Pagination";
+
+type employee = {
+  id: number;
+  employeeId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone?: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "employee ID",
+    accessor: "employeeId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const employeesListPage = () => {
+  const renderRow = (item: employee) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.photo}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.employeeId}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src="/delete.png" alt="" width={16} height={16}/>
+          </button>
+          
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
-    <div>employee page</div>
-  )
-}
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All employees</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          {/* <TableSearch /> */}
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaGreenLight">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaGreenLight">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaGreenLight">
+              <Image src="/create.png" alt="" width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={employeesData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+};
 
-export default EmployeePage
+export default employeesListPage;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,89 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { role } from "../lib/data";
+import { usePathname } from "next/navigation";
+
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.png",
+        label: "Home",
+        href: "/admin",
+        visible: ["admin", "employee", "project"],
+      },
+      {
+        icon: "/teacher.png",
+        label: "Employee",
+        href: "/employees",
+        visible: ["admin", "employee"],
+      },
+      {
+        icon: "/student.png",
+        label: "Projects",
+        href: "/projects",
+        visible: ["admin", "projects"],
+      },
+    ],
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.png",
+        label: "Profile",
+        href: "/profile",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/setting.png",
+        label: "Settings",
+        href: "/settings",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+      {
+        icon: "/logout.png",
+        label: "Logout",
+        href: "/logout",
+        visible: ["admin", "teacher", "student", "parent"],
+      },
+    ],
+  },
+];
+
+const Menu = () => {
+    const pathname = usePathname();
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+                const isActive = pathname === item.href;
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className={`flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight ${
+                    isActive ? "font-bold text-lamaBlack bg-lamaGreenLight" : ""
+                  }`}
+                
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
