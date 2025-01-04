6fcabdf17ae40e8ab3b82af76d423fbf8799c99c
Nguyễn Lâm Nhật <zznhatphilong@gmail.com>
2025-01-04 08:53:42 +0700

    done create project

---
+++ src/api/skills.ts
@@ -0,0 +1,17 @@
+import { supabase } from "../config/supabase";
+
+const getAllSkills = async () => {
+  try {
+    const { data, error } = await supabase.from("skills").select("*");
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching employees:", error);
+    return [];
+  }
+};
+
+
+export { getAllSkills };
